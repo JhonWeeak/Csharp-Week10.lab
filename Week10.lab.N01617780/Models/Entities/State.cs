35783c06c9eb3e1de02d9961e39c85210291e3d0
52687 <52687@HAOLIN>
2024-04-01 17:29:14 -0400

    Add project files.

---
+++ Week10.lab.N01617780/Models/Entities/State.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Week10.lab.N01617780.Models.Entities;
+
+public partial class State
+{
+    public string StateCode { get; set; } = null!;
+
+    public string StateName { get; set; } = null!;
+
+    public virtual ICollection<Customer> Customers { get; set; } = new List<Customer>();
+}
---
+++ Week10.lab.N01617780/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Week10.lab.N01617780.Models.Entities;
+class program
+{
+    public static void Main(string[] args)
+    {
+        using(var context = new MMABooksContext())
+        {
+            var customers = context.Customers;
+            foreach(var customer in customers)
+            {
+                Console.WriteLine(customer.Name);
+            }
+        }
+    }
+}
